22402d954c09006d3980e344a17513108cfb8b30
bharathjinka66 <bharathjinka66@gmail.com>
2018-03-22 16:34:16 +0530

    dscds

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
+from django.contrib import admin
 from . import views
+# from django.contrib.auth import views
 
 urlpatterns = [
+    url(r'^admin/', admin.site.urls),
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
@@ -9,5 +12,6 @@ urlpatterns = [
     url(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
     url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
     url(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
-
+    # url(r'^accounts/login/$', views.login, name='login'),
+    # url(r'', include('blog.urls')),
 ]
